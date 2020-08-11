f2590ec193664939575a12dd5cf2dba6192745e8
Josh <joshua.kujur98@gmail.com>
2020-08-11 20:43:38 +0530

    Update ezturtle.py

--- ezturtle.py
+++ ezturtle.py
@@ -5,7 +5,22 @@ s=turtle.Screen()
 s.bgcolor("black")
 t.speed(0)
 t.hideturtle()
-t.goto(75,120)
+def draw():
+    t.goto(75,120)
+    t.pencolor("gold")
+    design1()
+    t.pencolor("dark red")
+    t.penup()
+    t.forward(150)
+    t.pendown()
+    design2()
+    t.pencolor("grey")
+    t.penup()
+    t.right(30)
+    t.forward(150)
+    t.pendown()
+    design3()
+    turtle.done()
 def hex():
     for i in range(6):
         t.forward(50)
@@ -13,7 +28,7 @@ def hex():
 def hex_set():
     hex()
     for j in range(24):
-        t.right(15)
+        t.right(360/24)
         hex()
 def design1():
     hex_set()
@@ -23,10 +38,10 @@ def design1():
         t.forward(150)
         t.pendown()
         hex_set()
-dist1=math.sqrt(3)*150
 def design2():
     hex_set()
     t.right(30)
+    dist1=math.sqrt(3)*150
     for k in range(5):
         t.penup()
         t.right(60)
@@ -41,17 +56,4 @@ def design3():
         t.forward(300)
         t.pendown()
         hex_set()
-t.pencolor("gold")
-design1()
-t.pencolor("dark red")
-t.penup()
-t.forward(150)
-t.pendown()
-design2()
-t.pencolor("grey")
-t.penup()
-t.right(30)
-t.forward(150)
-t.pendown()
-design3()
-turtle.done()
+draw()
